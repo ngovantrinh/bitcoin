bc45790d8bfd085c9b92ff75f5f1fef81a072718
ngovantrinh <trinhtamtao@gmail.com>
2022-03-31 10:23:26 +0700

    fix

--- src/components/table/Selector/Selector.jsx
+++ src/components/table/Selector/Selector.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import * as KEY from '../../../constant/constant';
-import { getCoint } from '../../../features/coin/coinSlice';
 
 const Selector = ({ handleRate }) => {
   const option = KEY.SELECT_OPTION;
-  const dispatch = useDispatch();
 
   const exchangeRate = (item) => {
     // dispatch(getCoint(item));
